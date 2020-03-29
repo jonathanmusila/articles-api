4234f4337db8da87d59cf362b6a12dfa754c640b
jonathan <jonathan.muthiani@squaddigital.com>
2020-03-29 11:56:05 +0300

    added exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
+        // fix the 404 error 
+        if ($exception instanceof ModelNotFoundException) {
+            
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+
+        }
+
         return parent::render($request, $exception);
     }
 }
