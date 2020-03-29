a1dfd7caa6dfcc3c54980e60a53b7f0af3b3c0d3
jonathan <jonathan.muthiani@squaddigital.com>
2020-03-29 15:50:50 +0300

    added tests

---
+++ tests/Feature/Feature/LoginTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testRequiresEmailAndLogin()
+    {
+        $this->json('POST', 'api/login')
+            ->assertStatus(422)
+            ->assertJson([
+                'email' => ['The email field is required.'],
+                'password' => ['The password field is required.'],
+            ]);
+    }
+
+
+}
